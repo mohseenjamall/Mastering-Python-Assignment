750a3963f64f0accc18178501885e5a3c129c73d
Blaxk Raven <xmohsen50@gmail.com>
2024-11-12 15:17:38 +0200

    New Assignment 24 to 25 => Done

---
+++ 24 to 25.py
@@ -0,0 +1,25 @@
+#This file for the Assi 24 to 25 in lessons for Mastering Python
+
+# name = "osama",
+# print(name)
+# print(type(name)) #Print tuple without () => Done
+
+# friends = ("Osama", "Ahmed", "Sayed")
+# friends1 = list(friends) #Change the tuple to list.
+# friends1[0] = "Elzero" #Now you can change iteme in list becasue the tuple dosn't support to change it.
+# friends = tuple(friends1) #Then make the new varibale a tuple again.
+# print(friends) #Now print the first varibale it's a tuple.
+# print(type(friends)) #Chenk if it's a tuple or not 
+# print(len(friends)) #Check count of item on it. => Done
+
+# nums = (1, 2, 3)
+# letters = ("A", "B", "C")
+# sums = nums + letters
+# print(sums)
+# print(len(sums)) #Sum two tuples and len those => Done
+
+# my_tuple = (1, 2, 3, 4)
+# a, b, _, d = my_tuple #Make (_) ignore what you don't need.
+# print(a)
+# print(b)
+# print(d) #Print destruct => Done
