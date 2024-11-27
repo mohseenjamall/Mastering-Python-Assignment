2f8e6fa81196743eea9f05637137596f90caf534
Blaxk Raven <xmohsen50@gmail.com>
2024-11-27 18:05:22 +0200

    Lec 26 to 32 => Done

---
+++ 26 to 32.py
@@ -0,0 +1,39 @@
+#This file for the Assi 26 to 32 in lessons for Mastering Python
+
+# my_list = [1, 2, 3, 3, 4, 5, 1]
+# print(set(my_list))
+# print(type(my_list))
+# print(list(set(my_list))[0:4]) #Get unique value & type & last num => Done
+
+# nums = {1, 2, 3}
+# letters = {"A", "B", "C"}
+# nums.update(letters)
+# print(nums) #Merg two sets => Done
+
+# my_set = {1, 2, 3}
+# print(my_set)
+# my_set.clear()
+# print(my_set)
+# letters = {"A", "B"}
+# print(my_set.union(letters))
+# my_set.discard("c") #Print value not add without error use (Discard) => Done
+
+# set_one = {1, 2, 3}
+# set_two = {1, 2, 3, 4, 5, 6}
+# set_one.update(set_two)
+# print(bool(set_one)) #Merg V1 & V2 and Check if V1 is get all data True => Done
+
+# all = {
+#     "HTML" : {
+#         "progress" : "10%"
+#     },
+#     "CSS" : {
+#         "progress" : "20%"
+#     },
+#     "Python" : {
+#         "progress" : "90%"
+#     }
+# }
+# print(all)
+# all["AI"] = {"progress" : "10%"}
+# print(all)  #Make Dic and Update it => Done
