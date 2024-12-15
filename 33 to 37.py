1e48ab2d57e4c0d5c241be28940768b3bad8eb4e
Blaxk Raven <xmohsen50@gmail.com>
2024-12-15 07:21:59 +0200

    Assi 33 to 37 => Done

---
+++ 33 to 37.py
@@ -0,0 +1,30 @@
+#This file for the Assi 26 to 32 in lessons for Mastering Python
+
+# print(1 == 1) #True
+# print(1 < 2)  #True
+# print(2 > 1)  #True
+# print(3 > 2)  #True
+# Print("=" * 40)
+# print(1 == 0) #False
+# print(1 > 2)  #False
+# print(2 < 1)  #False
+# print(3 < 2)  #False    #Make 4 True & 4 False => Done
+
+# html = 80
+# css = 60
+# js = 70
+# print(html > 50 and css > 50 and js > 50) #Check if it Ture without use If Condition => Done
+
+# num_one = 10
+# num_two = 20
+# num = 20
+# print(num > num_one or num < num_two)
+# print(num < num_one and num < num_two) #Check if num < 2 valu or not => Done
+
+# num_one = 10
+# num_two = 20
+# result = num_one + num_two 
+# result **= 3 #
+# result %= 26000 
+# result /= 5
+# print(type(str(result))) #Operators => Done
