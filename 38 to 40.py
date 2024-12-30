a282137068cd2cac8014f687e89b7d052c68749e
Blaxk Raven <xmohsen50@gmail.com>
2024-12-30 10:34:00 +0200

    38 to 40 Assi => Done

---
+++ 38 to 40.py
@@ -0,0 +1,22 @@
+# This file for the Assi 38 to 40 in lessons for Mastering Python
+
+# fName = "     moHsEn   "
+# mName = "       Jamal"
+# lName = "Elshirbiny     "
+# fName = fName.strip().capitalize()
+# mName = mName.strip().capitalize()
+# lName = lName.strip().capitalize()
+#.1s to get first chr in vari.
+# print(f"hello, {fName} {mName:.1s} {lName}, happy To see you here.") #rm spaces & Capialize first chr => Done
+
+# fAge = input('what\'s your age ?').strip().capitalize()
+# fAge = int(fAge)
+# if fAge < 16:
+#     print("Hello Your Age Is Under 16, Some Articles Is Not Suitable For You")
+# else:
+#     print(f"Hello Your Age Is {fAge}, All Articles Is Suitable For You") #Check if age under 16 => Done
+
+# fName = "osama".capitalize()
+# email = "Osama@Nn.Sa"
+
+# print(f"Your Name Is {fName} \nEmail Service Provider Is {email[email.index('@')+1 :8].lower()} \nTop Level Domain Is {email[email.index('.')+1 :12].lower()}") # Split => Done
