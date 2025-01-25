afcd458630403e07ed4ea253c879d858c66a933f
Blaxk Raven <xmohsen50@gmail.com>
2025-01-25 13:56:31 +0200

    Assi 41 to 46 => Done

---
+++ 41 to 46.py
@@ -0,0 +1,37 @@
+# This file for the Assi 41 to 46 in lessons for Mastering Python
+
+# num1 = int(20)
+# num2 = int(40)
+# operation = "+"
+# print(num1,operation,num2)
+# if operation == "-":
+#     result= num1 - num2
+#     print(f"[num1 {num1}] + [num2 {num2}] = {result}")
+# else:
+#     result2= num1 * num2
+#     print(f"[num1 {num1}] * [num2 {num2}] = {result2}") # Done
+
+# age = 9
+# print("App Is Suitable For You" if age > 17 else "App Is Not Suitable For You") #Ternary Conditional Operator => Done
+
+# age = int(input("Please Write Your Age: ").strip())
+
+# months = age * 12
+# weeks = months * 4
+# days = age * 365
+# hours = age * 24
+# minutes = age * 60
+# seconds = age * 60
+
+# if age > 10 or age < 100: # Make Calc Age => Done
+#     print(f"Your Age In Months Is {months:,}\nYour Age In Weeks Is {weeks:,}\nYour Age In Days Is {days:,}\nYour Age In Hours Is {hours:,}\nYour Age In Minutes Is {minutes:,}\nYour Age In Seconds Is {seconds:,}")
+
+# country = input("Input Your Country: ").capitalize().strip()
+# countries = ["Egypt", "Palestine", "Syria", "Yemen", "KSA", "USA", "Bahrain", "England"]
+# price = 100
+# discount = 30
+# if country in countries:
+#     discounted_price = price - (price * (discount / 100))
+#     print(f"Your Country {country} Is Eligible For Discount And The Price After Discount Is ${discounted_price:.2f}")
+# else:
+#     print(f"Your Country {country} Is Not Eligible For Discount And The Price Is ${price}") #Discounts => Done
