0942d0e9535c478c0d18a3ea4db693aac54cf239
Blaxk Raven <xmohsen50@gmail.com>
2025-02-17 16:22:57 +0200

    51 to 55 Assignment => Done

---
+++ 51 t 55.py
@@ -0,0 +1,7 @@
+# This file for the Assi 51 to 55 in lessons for Mastering Python
+
+my_nums = [15, 81, 5, 17, 20, 21, 13]
+
+for num in my_nums:
+    if num % 5 == 0:
+        print(f"the Numbers {num}")
